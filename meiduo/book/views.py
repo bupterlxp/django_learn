9e627720c61d6306ec1a9314869cc1c337959f13
lxp <lxp@bupt.edn.cn>
2024-10-11 16:14:11 +0800

    定义视图函数

--- meiduo/book/views.py
+++ meiduo/book/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+"""
+视图
+其实就是python函数
+视图函数有两个要求：
+    1.视图函数的第一个参数就是接受请求。这个请求就是http的HttpRequest类对象
+"""
+from django.http import HttpRequest
+from django.http import HttpResponse
+
+
+# 我们期望用户输入 http://127.0.0.1:8000/index
+# 来访问视图函数
+
+def index(request):
+    return HttpResponse("OK呀")
