b756ac20fc34c5f8123ee2d800934dfbe92660fa
lxp <lxp@bupt.edn.cn>
2024-10-11 17:59:16 +0800

    Merge remote changes

--- meiduo/meiduo/settings.py
+++ meiduo/meiduo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'book'  # 方案一
-    # "book.apps.Bookconfig" # 方案二
+    # 'book'  # 方案一
+    "book.apps.BookConfig" # 方案二
 ]
 
 MIDDLEWARE = [
@@ -114,7 +115,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+# 告知静态资源的位置
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
